47a0420190394d881f885689f055533d4b89373f
ssavel19 <ssavel19@bw.edu>
2023-04-18 14:45:12 -0400

    :3

--- app/src/main/java/com/example/reminderlyfinalproject/model/Reminder.java
+++ app/src/main/java/com/example/reminderlyfinalproject/model/Reminder.java
@@ -1,4 +1,6 @@
 package com.example.reminderlyfinalproject.model;
 
 public class Reminder {
+    public int reminderId;
+    public String reminderName, reminderLocation, reminderDate, reminderTime;
 }
