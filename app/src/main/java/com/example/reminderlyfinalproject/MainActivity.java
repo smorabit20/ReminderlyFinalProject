47701e6952f622d7321628bfc76053ce7683b77a
smorabit20 <smorabit20@bw.edu>
2023-04-18 14:31:35 -0400

    brian

--- app/src/main/java/com/example/reminderlyfinalproject/MainActivity.java
+++ app/src/main/java/com/example/reminderlyfinalproject/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // hwllo from brian
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
