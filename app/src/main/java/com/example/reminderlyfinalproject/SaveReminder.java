ccaa2f1ca593f92e7a28f050b63f6b2a69a4045a
smorabit20 <smorabit20@bw.edu>
2023-04-20 22:15:56 -0400

    Save Reminder Btn

--- app/src/main/java/com/example/reminderlyfinalproject/SaveReminder.java
+++ app/src/main/java/com/example/reminderlyfinalproject/SaveReminder.java
@@ -3,6 +3,7 @@ package com.example.reminderlyfinalproject;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -59,6 +60,18 @@ public class SaveReminder extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_save_reminder, container, false);
+        View view = inflater.inflate(R.layout.fragment_save_reminder, container, false);
+
+        //BUTTONS
+
+        //ADD REMINDER BUTTON
+        view.findViewById(R.id.createReminderBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_saveReminder_to_viewReminders);
+            }
+        });
+
+        return view;
     }
 }
